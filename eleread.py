b3e79b58fd4e9b099d399602e26f8fb3fff79522
Alexander A Swenson <aaswenson@wisc.edu>
2016-04-11 17:54:10 -0500

    module to read element library

---
+++ eleread.py
@@ -0,0 +1,10 @@
+# This module reads an element library into a single data structure
+# This module assumes no blank lines or comments in the library
+
+import re
+
+file = open('elelib.std')
+
+for lines in file:
+     print line
+
