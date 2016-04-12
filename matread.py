9d385904b0b829c5f412fa70c9a9ce201573a5fe
Alexander A Swenson <aaswenson@wisc.edu>
2016-04-12 04:56:56 -0500

    tested code for processing material library data

---
+++ matread.py
@@ -0,0 +1,30 @@
+# This script parses a material database
+
+import re 
+
+file = open('matlib.std')
+
+# function to parse through the first line of text
+
+def first_parse(line):
+    data = line.split()
+    return(data)
+
+def data_par(file):   
+    material_dat = {} 
+    for line in file:
+        data = first_parse(line)  
+        if len(data[0]) > 2:
+            alloy = data[0]
+            density = data[1]
+            num_of_elements = data[2]
+            comp_dat = {}
+        else:
+            element = data[0]
+            mass_frac = data[1]
+            atomic_number = data[2]
+            comp_dat[element]={'Atomic Number':atomic_number,'Mass Fraction':mass_frac}
+        material_dat[alloy]={'Density':density,'Number of Elements':num_of_elements,'Composition Data':comp_dat}
+    return(material_dat)
+
+
---
+++ testmat.py
@@ -0,0 +1,25 @@
+# These tests verify the material parser script performance
+from matread import data_par
+from nose.tools import assert_equal
+from nose.tools import assert_raises
+
+def density_test():
+    file = open('matlib.std')
+    data = data_par(file)
+    exp = str('1.00')
+    obs = data['water']['Density']
+    assert_equal(obs,exp)
+
+def massfrac_test():
+    file = open('matlib.std')
+    data = data_par(file)
+    exp = str('0.73000e+00')
+    obs = data['WALLOY']['Composition Data']['fe']['Mass Fraction']
+    assert_equal(obs,exp)
+
+def number_elements_test():
+    file = open('matlib.std')
+    data = data_par(file)
+    exp = str('9')
+    obs = data['R-EPXY']['Number of Elements']
+    assert_equal(obs,exp)
